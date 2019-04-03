44bf84f4a9c174541717d0326697b0f9ece9da8d
Evgeniy Hryshchenko <evgeniy.hryshchenko@gmail.com>
2019-04-03 14:04:50 +0300

    homework:obj

---
+++ objects-and-arrays/src/js/script.js
@@ -0,0 +1,18 @@
+alert('Возведение элемнтов массива в куб.');
+
+let arr=[6,2,4];
+
+function getCube(x) {
+return x*x*x;
+}
+
+function map(fn,array) {
+let mainArr = [];
+for(let i = 0;i<array.length;i++) {
+mainArr[i]=fn(arr[i]);
+}
+return 'Новый массив: ' + mainArr;
+}
+
+alert(map(getCube,arr));
+alert('Старый массив: '+arr);
