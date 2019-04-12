cd23bf0c621cb3c1282911e2f9db323d7b32a60a
Evgeniy Hryshchenko <evgeniy.hryshchenko@gmail.com>
2019-04-12 23:14:40 +0300

    message

--- tabs-js/app/js/script.js
+++ tabs-js/app/js/script.js
@@ -6,3 +6,36 @@
 		$(".section__tab-item").hide().eq($(this).index()).fadeIn()
 	}).eq(0).addClass("active");
 })(jQuery);
+;(function(){
+  "use strict";
+
+  let diagram = document.querySelector(".section__diagram");
+  let rating = +document.querySelector('.section__rating').innerText;
+  function showDiagram(val,maxVal) {
+const maxValue = maxVal;
+const remain = maxValue - val;
+var oilData = {
+    datasets: [
+        {
+            data: [val,remain],
+            backgroundColor: [
+                "#1ae780",
+                "#212534"
+            ],
+            borderWidth: 0
+        }]
+};
+var pieChart = new Chart(diagram, {
+  type: 'pie',
+  data: oilData,
+  options: {
+  	legend: {
+      display: false
+    },
+    tooltips: {enabled: false},
+    hover: {mode: null},
+  }
+});
+}
+showDiagram(rating,12);
+})();
--- test/app/js/script.js
+++ test/app/js/script.js
@@ -1,3 +1,34 @@
-;(function IIFE(){
-
-})();
+/* canvas.width = canvas.offsetWidth;
+canvas.height = canvas.offsetHeight; */
+let diagram = document.querySelector(".section__diagram");
+/* let ctx = diagram.getContext("2d");
+ctx.canvas.width = 60;
+ctx.canvas.height = 60;
+ctx.render(); */
+function showDiagram(val,maxVal) {
+const maxValue = maxVal;
+const remain = maxValue - val;
+var oilData = {
+    datasets: [
+        {
+            data: [val,remain],
+            backgroundColor: [
+                "#FF6384",
+                "#212534"
+            ],
+            borderWidth: 0
+        }]
+};
+var pieChart = new Chart(diagram, {
+  type: 'pie',
+  data: oilData,
+  options: {
+  	legend: {
+      display: false
+    },
+    tooltips: {enabled: false},
+    hover: {mode: null},
+  }
+});
+}
+showDiagram(7.8,12);
