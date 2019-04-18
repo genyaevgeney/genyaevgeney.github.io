41647dc27f53bf075f954aa59185660e383a2220
Evgeniy Hryshchenko <evgeniy.hryshchenko@gmail.com>
2019-04-19 02:30:32 +0300

    slider

---
+++ slider-js/app/js/script.js
@@ -0,0 +1,13 @@
+;(function($){
+	"use strict";
+
+	$(function(){
+		$('.section__slider').slick({
+			slidesToShow: 4,
+			slidesToScroll: 1,
+			// centerMode: true,
+			// dots: true,
+			// focusOnSelect: true
+		});
+	})
+})(jQuery);
